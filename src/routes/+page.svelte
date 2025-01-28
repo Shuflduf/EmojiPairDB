033d7a01bb644975f9efef6787dc2e4f18b9e2bf
Shuflduf <shuflduf@gmail.com>
2025-01-27 21:19:52 -0700

    basic setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
+<script lang="ts">
+  import { Client, Databases, ID } from "appwrite";
+
+  const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject("679855a20001624a7a0b");
+
+  const databases = new Databases(client);
+
+  async function createDocument() {
+    const result = await databases.createDocument(
+      "main",
+      "users",
+      ID.unique(),
+      {
+        username: "JOSHUA",
+        emojis: "🐔🐴",
+      }
+    );
+
+    console.log(result);
+  }
+
+  createDocument();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
